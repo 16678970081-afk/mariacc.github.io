9e99fa16dcda6fa57e833d718d42df65ae8c1a29
Maria <maria@MariadeMacBook-Air.local>
2026-05-02 14:35:41 +0800

    remove base

--- astro.config.ts
+++ astro.config.ts
@@ -22,7 +22,7 @@ import type { PluginOption } from 'vite'
 
 export default defineConfig({
   site: 'https://16678970081-afk.github.io',
-  base: '/mariacc.github.io',
+  // base: '/mariacc.github.io',
   // site: 'https://astro-erudite.vercel.app',
   integrations: [mdx(), react(), sitemap(), icon()],
   vite: {
